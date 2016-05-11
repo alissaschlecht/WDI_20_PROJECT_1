9497a35e15c9f8a92204b43aa1242b0ced96bfcf
Alissa Schlecht <schlecht220@gmail.com>
2016-05-11 09:33:04 +0100

    game over function, final version to present

--- index.js
+++ index.js
@@ -11,60 +11,132 @@ $(function() {
   var playerLives = $("#lives");
   var playerLives1 = 3;
   var playerLives2 = 3;
+  var player = 1;
+  var playerScore = [0,0];
   var Level = $('#level');
-  var road = $('#road');
   var roadWidth = $('.road').outerWidth(true);
-  var carInterval;
-  // var logInterval;
-  var rate = 0;//1400;
-  var speed = 0;//3000;
+  var animationInterval;
+  var collisionInterval;
+  var generateInterval;
+  var rate = 4000;
+  var frameRate = 50;
+  var creationRate = 1800;
+  var moveUp = false;
+  var moveDown = false;
+  var moveLeft = false;
+  var moveRight = false;
+  var car = $('.allCars');
+  var leftItems = $('.carMoveRight');
+  var rightItems = $('.carMoveLeft');
+  var frogSpeed = "5px";
+  var carSpeed = "3px";
 
 
-// =====================  Start Screen ================================ //
+// =====================  Start Screen ======================== //
 
 
-$('#start').click(function() {
-    $('#splashscreen').animate({height: 0, opacity: 0}, function() {
-        $(this).remove();
-    });
-});
- 
+  $('#start').click(function() {
+      $('#splashscreen').animate({height: 0, opacity: 0}, function() {
+          $(this).remove();
+      });
 
-//===================== Nest all functions in one ===============================//
+  });
+
+//===================== Play game ==================//
   
   play();
 
   function play(){
 
-    clearInterval(carInterval);
-    carInterval = setInterval(carLoop, rate);
-    gameOver();
+    clearInterval(animationInterval);
+    animationInterval = setInterval(animate, frameRate);
+    clearInterval(collisionInterval);
+    collisionInterval = setInterval(collisionCheck, frameRate);
+    clearInterval(generateInterval);
+    generateInterval = setInterval(createCar, creationRate);
     moveFrog();
     playThreeLives();
+
   }
 
+  function animate() {
+
+      // animte frog
+      if(moveLeft) {
+          frog.css({
+              left: '-=' + frogSpeed +''
+          }); //left arrow key
+      }
+      if(moveUp) {
+          //winnerArea();
+          frog.css({
+              top: '-=' + frogSpeed +''
+          }); //up arrow key
+      }
+      if(moveRight) {
+          frog.css({
+              left: '+=' + frogSpeed +''
+          }); //right arrow key
+      }
+      if(moveDown) {
+          frog.css({
+              top: '+=' + frogSpeed +''
+          }); //bottom arrow key
+      }
+
+      // animate cars
+      car = $('.allCars');
+      leftItems = $('.carMoveRight');
+      rightItems = $('.carMoveLeft');
+
+      leftItems.each(function(index, car){
+
+          $(car).css({left: "+=" + carSpeed + ""});
+
+          if(parseInt($(car).css('left')) > parseInt($(".wrapper").css('width'))) {
+
+              $(car.remove());
+
+          }
+
+      });
+
+      rightItems.each(function(index, car){
+
+          $(car).css({right: "+=" + carSpeed+ ""});
+
+          if(parseInt($(car).css('right')) > parseInt($(".wrapper").css('width'))) {
+
+              $(car.remove());
+
+          }
+
+      });
+
+  }
 
 // ======================== Switch players and update scores =============== //
 
-  var player = 1;
-  var playerScore = [0,0];
 
   function updatePlayerScore() {
       if (player === 1){
         playerScore[0]++;
-        $('#player1').append('<img id="scoreFrog" src="froggerHealthy.png"/>');
+        $('#player1').append('<img id="scoreFrog" src="images/froggerHealthy.png"/>');
         scoreOne.html(playerScore[0]);
         playThreeLives();
+        gameOver();
    
       }else{
         playerScore[1]++;
-        $('#player2').append('<img id="scoreFrog" src="froggerHealthy.png"/>');
+        $('#player2').append('<img id="scoreFrog" src="images/froggerHealthy.png"/>');
         scoreTwo.html(playerScore[1]);
         playThreeLives();
+        gameOver();
       }
     } 
 
   function playThreeLives() {
+
     if (player ===1 && playerLives1 === 0){
       player = 2;
       playerLives2 = 3;
@@ -76,151 +148,128 @@ $('#start').click(function() {
       playerLives1 = 3;
       showTurn.html("Player 1: Your Turn! <br> Lives: </br>")
       frog.css({"left": roadWidth*.45, "top": "40px"});      
+    
     }
   }
 
   function gameOver() {
 
-    if (playerScore[0] === 1){
-      console.log(works);
-      $("#gameover1").fancybox().click();
+    if ((playerScore[0]) == 3) {
+      console.log("works");
+      $("#gameover1").css("display" , "block");
+      clearInterval(generateInterval);
+
     }
-    else if (playerScore[1] === 1){
-      $("#gameover2").fancybox().click();
+    else if (playerScore[1] == 3){
+       console.log("works");
+       $("#gameover2").css("display" , "block");
+       clearInterval(generateInterval);
+
     }
-    else{}
+
   }
-
-  //========================== Winner ==============================================//
-    
-    function winnerArea() {
-
-      var winArea = $('#winarea');
-      var winTop = winArea.offset().top;
-      var winHeight = winArea.outerHeight(true) + 20;
-      var winBottom = winTop + winHeight;
-
-
-      var frogTop = frog.offset().top;
-      var frogHeight = frog.outerHeight(true);
-      var frogBottom = frogTop + frogHeight;
-
-      if (frogBottom <= winBottom){
-        updatePlayerScore();
-        frog.stop().css({"left": roadWidth*.45, "top": "40px"});
-      }
-
-    }
 
 
   //=============Car movement and collision detection===================//
 
   $('#easy').click(function(){
-    rate = 10000;
-    speed = 1000;
+
+    
+    carSpeed = "5px";
+    creationRate = 2000;
+    clearInterval(generateInterval);
+    generateInterval = setInterval(createCar, creationRate)
+  
   });
 
   $('#medium').click(function(){
-    rate = 1000;
-    speed = 4000;
+    
+    carSpeed = "5px";
+    creationRate = 1500;
+    clearInterval(generateInterval);
+    generateInterval = setInterval(createCar, creationRate)
+   
+
   });
   
   $('#hard').click(function(){
     
-    rate = 100;
+    carSpeed = "10px";
+    creationRate = 1000;
+    clearInterval(generateInterval);
+    generateInterval = setInterval(createCar, creationRate);
 
-    var randomNum = Math.random();
-    if (randomNum < .66){
-      speed = 900;
-    }else{
-      speed = 1000;
-    }
   });
 
 
 
-  function carLoop() {
+  function createCar() {
+      
+      $('.first').prepend('<div class="carMoveRight allCars"><img src="images/unicycle.gif"/></div>');
+      $('.first').append('<div class="carMoveLeft  allCars"><img src="images/scooter.gif"/></div>');
 
-      var collisionInterval;
-      clearInterval(collisionInterval);
-      collisionInterval = setInterval(collisionCheck, 30);
+      $('.second').prepend('<div class="carMoveRight allCars"><img src="images/cycleOne.gif"/></div>');
+      $('.second').append('<div class="carMoveLeft  allCars"><img src="images/redcar.png"/></div>');
 
-      $('.first').prepend('<div class="carMoveRight allCars"><img src="unicycle.gif"/></div>');
-      $('.first').append('<div class="carMoveLeft  allCars"><img src="scooter.gif"/></div>');
-
-      $('.second').prepend('<div class="carMoveRight allCars"><img src="cycleOne.gif"/></div>');
-      $('.second').append('<div class="carMoveLeft  allCars"><img src="redcar.png"/></div>');
-
-      var car = $('.allCars');
-      var leftItems = $('.carMoveRight');
-      var rightItems = $('.carMoveLeft');
-
-      setTimeout(function() {
-        $.each(leftItems, function(i, e) {
-          $(e).animate({
-            left: + roadWidth*.95
-          }, speed, function() {
-              $(this).remove();
-          });
-        })
-      });
-
-      setTimeout(function() {
-        $.each(rightItems, function(i, e) {
-          $(e).animate({
-            right: + roadWidth*.95
-          }, speed, function() {
-              $(this).remove();
-          });
-        })
-      });
-
-      function collision(car, frog) {
-
-        var carLeft = car.offset().left;
-        var carTop = car.offset().top;
-        var carHeight = car.outerHeight(true);
-        var carWidth = car.outerWidth(true);
-        var carBottom = carTop + carHeight ;
-        var carRight = carLeft + carWidth;
-        var frogLeft = frog.offset().left;
-        var frogTop = frog.offset().top;
-        var frogHeight = frog.outerHeight(true);
-        var frogWidth = frog.outerWidth(true);
-        var frogBottom = frogTop + frogHeight;
-        var frogRight = frogLeft + frogWidth; 
-
-        if (carBottom < frogTop || carTop > frogBottom || carRight < frogLeft || carLeft > frogRight) {
-        }else {
-          if (player === 1){
-            crushFrog();
-            playerLives1 = parseInt(playerLives1) - 1;
-            playThreeLives();
-            playerLives.html(playerLives1);
-
-          }else{
-            crushFrog();
-            playerLives2 = playerLives2 - 1;
-            playThreeLives();
-            playerLives.html(playerLives2);
-          }
-        }
-      }
-
-      function collisionCheck(){
-        car.each(function(index, element){
-          collision($(element), frog);
-        });
-      }             
   }
 
+  function collision(car, frog) {
+
+    var carLeft = car.offset().left;
+    var carTop = car.offset().top;
+    var carHeight = car.outerHeight(true);
+    var carWidth = car.outerWidth(true);
+    var carBottom = carTop + carHeight ;
+    var carRight = carLeft + carWidth;
+    var frogLeft = frog.offset().left;
+    var frogTop = frog.offset().top;
+    var frogHeight = frog.outerHeight(true);
+    var frogWidth = frog.outerWidth(true);
+    var frogBottom = frogTop + frogHeight;
+    var frogRight = frogLeft + frogWidth;
+    var winArea = $('#winarea');
+    var winTop = winArea.offset().top;
+    var winHeight = winArea.outerHeight(true) + 20;
+    var winBottom = winTop + winHeight; 
+
+    if (carBottom < frogTop || carTop > frogBottom || carRight < frogLeft || carLeft > frogRight) {
+    }else {
+      if (player === 1){
+        crushFrog();
+        playerLives1 = parseInt(playerLives1) - 1;
+        playThreeLives();
+        playerLives.html(playerLives1);
+
+      }else{
+        crushFrog();
+        playerLives2 = playerLives2 - 1;
+        playThreeLives();
+        playerLives.html(playerLives2);
+      }
+    }
+
+    if (frogBottom <= winBottom){
+      updatePlayerScore();
+      crushFrog();
+    }
+  }
+
+  function collisionCheck(){
+    car.each(function(index, element){
+      collision($(element), frog);
+    });
+  } 
+
   function crushFrog() {
-    var alive = "froggerHealthy.png";
-    var dead = "froggerCrushed.svg";        
+    var alive = "images/froggerHealthy.png";
+    var dead = "images/greencircle.png";        
     $this = $("#frog img");            
 
     $this.animate('src', dead);
-    frog.stop().css({"left": roadWidth*.45, "top": "40px"});     
+    frog.css({
+      "top": "10px",
+      "left": "180px"
+    })
     $this.attr('src', alive);
 
   };
@@ -228,142 +277,47 @@ $('#start').click(function() {
 //================================= Move Frog ==================================//
 
   function moveFrog () {
+
     $(document).keydown(function(e) {
+
       switch (e.which) {
+
         case 37:
-            frog.stop().animate({
-                left: '-=25px'
-            }); //left arrow key
+            moveLeft = true;
             break;
         case 38:
-            winnerArea();
-            frog.stop().animate({
-                top: '-=25px'
-            }); //up arrow key
+            moveUp = true; //up arrow key
             break;
         case 39:
-            frog.stop().animate({
-                left: '+=25px'
-            }); //right arrow key
+            moveRight = true; //right arrow key
             break;
         case 40:
-            frog.stop().animate({
-                top: '+=25px'
-            }); //bottom arrow key
+            moveDown = true;//bottom arrow key
             break;
+        
+        }
+    });
+
+    $(document).keyup(function(e) {
+
+      switch (e.which) {
+
+        case 37:
+            moveLeft = false;
+            break;
+        case 38:
+            moveUp = false; //up arrow key
+            break;
+        case 39:
+            moveRight = false; //right arrow key
+            break;
+        case 40:
+            moveDown = false;//bottom arrow key
+            break;
+        
         }
     });
 
   }
-
-
-
-
-
-
-
-  //============= Logs ========================= //
-
-  // function logLoop() {
-
-  //     var collisionInterval;
-  //     clearInterval(collisionInterval);
-  //     collisionInterval = setInterval(collisionCheck, 30);
-
-  //     $('.stream').prepend('<div class="logMoveRight allLogs"><img src="car2.png"/></div>');
-  //     $('.stream').append('<div class="logMoveLeft  allLogs"><img src="car1.png"/></div>');
-
-  //     var log = $('.allLogs');
-  //     var leftItems = $('.logMoveRight');
-  //     var rightItems = $('.logMoveLeft');
-  //     var stream = $('#stream1');
-
-  //     setTimeout(function() {
-  //       $.each(leftItems, function(i, e) {
-  //         $(e).animate({
-  //           left: + roadWidth*.95
-  //         }, speed, function() {
-  //             $(this).remove();
-  //         });
-  //       })
-  //     });
-
-  //     setTimeout(function() {
-  //       $.each(rightItems, function(i, e) {
-  //         $(e).animate({
-  //           right: + roadWidth*.95
-  //         }, speed, function() {
-  //             $(this).remove();
-  //         });
-  //       })
-  //     });
-
-  //     function collision(log, frog) {
-
-  //       var logLeft = log.offset().left + 40;
-  //       var logTop = log.offset().top + 40;
-  //       var logHeight = log.outerHeight(true);
-  //       var logWidth = log.outerWidth(true);
-  //       var logBottom = logTop + logHeight;
-  //       var logRight = logLeft + logWidth;
-  //       var frogLeft = frog.offset().left;
-  //       var frogTop = frog.offset().top;
-  //       var frogHeight = frog.outerHeight(true);
-  //       var frogWidth = frog.outerWidth(true);
-  //       var frogBottom = frogTop + frogHeight;
-  //       var frogRight = frogLeft + frogWidth; 
-  //       var streamTop = stream.offset().top;
-  //       var streamHeight = stream.outerHeight(true);
-  //       var streamBottom = streamTop + streamHeight;
-
-
-  //       if (frogTop < logBottom){
-
-  //         console.log(frogTop);
-  //         frogTop = logTop;
-  //         console.log(frogTop);
-  //       }
-
-
-
-
-
-        // if ((frogBottom < streamBottom) && (logBottom < frogTop || logTop > frogBottom || logRight < frogLeft || logLeft > frogRight)) {
-        //   if (player === 1){
-        //     crushFrog();
-        //     playerLives1 = parseInt(playerLives1) - 1;
-        //     playThreeLives();
-        //     playerLives.html(playerLives1);
-
-        //   }else{
-        //     crushFrog();
-        //     playerLives2 = playerLives2 - 1;
-        //     playThreeLives();
-        //     playerLives.html(playerLives2);
-        //   }
-        // }
-  //     }
-
-  //     function collisionCheck(){
-  //       log.each(function(index, element){
-  //         collision($(element), frog);
-  //       });
-  //     }             
-  // }
-
-
-  // function crushFrog() {
-  //   var alive = "froggerHealthy.png";
-  //   var dead = "froggerCrushed.svg";        
-  //   $this = $("#frog img");            
-
-  //   $this.animate('src', dead);
-  //   frog.stop().css({"left": roadWidth*.45, "top": "40px"});     
-  //   $this.attr('src', alive);
-
-  // };
-
-
-  
 
   });
